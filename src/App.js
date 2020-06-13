2f58092f3622dea45d667464fcda80f6034c4e5d
mbajoshuac <angelicjoekeyz@gmail.com>
2020-06-13 22:53:53 +0100

    searchbox filter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box-component';
 
 class App extends Component {
   constructor() {
@@ -20,16 +21,17 @@ class App extends Component {
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-    )
+      monster.name.toLowerCase().includes(searchField.toString().toLowerCase())
+    );
     return (
       <div className="App">
-        <input type="search"
-          placeholder="Search Monster"
-          onChange={e => {
+        < SearchBox
+          placeholder='Search Monster'
+          handleChange={e => {
             this.setState({ searchField: e.target.value }, console.log(e.target.value));
-          }} />
-        < CardList monsters={this.state.monsters} />
+          }}
+        />
+        < CardList monsters={filteredMonsters} />
 
       </div >
     )
---
+++ src/student.js
@@ -0,0 +1,10 @@
+//nth_most_rate
+const rate = [5, 4, 5, 4, 5, 4, 4, 5, 3, 3, 3, 2, 2, 1, 5]
+
+//function
+const nth_most_rate = (rate) => {
+
+    console.log(rate);
+
+
+}
