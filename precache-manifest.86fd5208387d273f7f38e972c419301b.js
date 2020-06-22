0d0787caa18d4730f65c901d1341f5babbcb760c
mbajoshuac <angelicjoekeyz@gmail.com>
2020-06-22 22:19:44 +0100

    Updates

---
+++ precache-manifest.86fd5208387d273f7f38e972c419301b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888fd10dbc1f82f917445bc5a5c23653",
+    "url": "/monsteroledex/index.html"
+  },
+  {
+    "revision": "f271751a40465ff47a12",
+    "url": "/monsteroledex/static/css/main.8425f184.chunk.css"
+  },
+  {
+    "revision": "aa86dba7ccdda618ee4d",
+    "url": "/monsteroledex/static/js/2.f5912c83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsteroledex/static/js/2.f5912c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f271751a40465ff47a12",
+    "url": "/monsteroledex/static/js/main.35945f2a.chunk.js"
+  },
+  {
+    "revision": "64f00fc95ebc01066f93",
+    "url": "/monsteroledex/static/js/runtime-main.cc578011.js"
+  }
+]);
